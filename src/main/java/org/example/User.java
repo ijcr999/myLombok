aaa8e0255c2ccf513e6c3b2e410929d85ae533da
Andrzej Babets <black@Andrzejs-Air.lan>
2024-01-22 18:36:20 +0100

    First commit

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Builder
+//@NoArgsConstructor
+@AllArgsConstructor()
+
+public class User {
+
+
+    //    @NonNull
+    private int age2;
+    private int age1;
+    private int age3;
+    private int age4;
+    private int age5;
+    private int age8;
+    private int age0;
+    private int age9;
+    private int age6;
+    private String name;
+    private String lastName;
+}
